4bddf78e2258141f6140b7a4f05810b70c32edbd
cecsun <ceciliesunde94@gmail.com>
2024-01-23 20:46:41 +0100

    Added cypress and e2e tests for login/logout

---
+++ cypress/e2e/3-testing/login.cy.js
@@ -0,0 +1,47 @@
+describe("Login", () => {
+  it("logs in with valid credentials", () => {
+    // const username = "cecsun";
+
+    cy.visit("/");
+    cy.wait(1000);
+    cy.get('form[id="registerForm"').within(() => {
+      cy.get('[data-auth="login"]').click();
+    });
+    cy.wait(1000);
+    cy.get('form[id="loginForm"').within(() => {
+      cy.get('input[id="loginEmail"]').type(Cypress.env("email"));
+      cy.wait(100);
+      cy.get('input[id="loginPassword"]').type(Cypress.env("password"));
+      cy.wait(100);
+
+      cy.get('button[type="submit"]').click();
+    });
+    cy.url().should(
+      "include",
+      "/?view=profile&name=" + Cypress.env("username"),
+    );
+  });
+
+  it("logs in with invalid credentials", () => {
+    cy.visit("/");
+    cy.wait(1000);
+    cy.get('form[id="registerForm"').within(() => {
+      cy.get('[data-auth="login"]').click();
+    });
+    cy.wait(1000);
+    cy.get('form[id="loginForm"').within(() => {
+      cy.get('input[id="loginEmail"]').type(Cypress.env("email"));
+      cy.wait(100);
+      cy.get('input[id="loginPassword"]').type("wrongpassword");
+      cy.wait(100);
+
+      cy.get('button[type="submit"]').click();
+    });
+    cy.on("window:alert", (t) => {
+      //assertions
+      expect(t).to.equal(
+        "Either your username was not found or your password is incorrect",
+      );
+    });
+  });
+});
